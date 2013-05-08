33384a2c1e72ab4e614c70ce7d797962d155d867
Anthony Cromartie <cromarti@arctic.cse.msu.edu>
2013-05-07 20:17:56 -0400

    binStff

---
+++ bin/client.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = int(sys.argv[1])               # take the port in from cmd linecd
+
+
+print 'connecting...'
+s.connect((host, port))
+print 'got:'
+print s.recv(1024)
+print 'and next:'
+print s.recv(1024)
+print 'done.'
+s.close()
